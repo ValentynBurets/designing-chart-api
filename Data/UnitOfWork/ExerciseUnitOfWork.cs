c9eff8870a6072775d73bab76442fad5408fb0a9
valentyn.burets <Valentyn.Burets@gmail.com>
2021-11-22 00:28:11 +0200

    added UnitOfWork

---
+++ Data/UnitOfWork/ExerciseUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Data.Contract.UnitOfWork;
+using Data.EF;
+using Data.Repository;
+using Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.UnitOfWork
+{
+    public class ExerciseUnitOfWork : IExerciseUnitOfWork
+    {
+        private readonly DomainDbContext _dbContext;
+        private IAttemptRepository _attemptRepository;
+        private ICategoryRepository _categoryRepository;
+        private IExerciseRepository _exerciseRepository;
+        
+        public ExerciseUnitOfWork(DomainDbContext exercisesDbContext)
+        {
+            _dbContext = exercisesDbContext;
+        }
+
+
+        public IAttemptRepository AttemptRepository => _attemptRepository ??= new AttemptRepository(_dbContext);
+
+        public ICategoryRepository CategoryRepository => _categoryRepository ??= new CategoryRepository(_dbContext);
+
+        public IExerciseRepository ExerciseRepository => _exerciseRepository ??= new ExerciseRepository(_dbContext);
+
+        public async Task<int> Save() => await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ designing-chart-api/Configurations/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Data.Contract.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace designing_chart_api.Configurations
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection service)
+        {
+            service.AddTransient<IExerciseUnitOfWork, ExerciseUnitOfWork>();
+
+            return service;
+        }
+    }
+}
