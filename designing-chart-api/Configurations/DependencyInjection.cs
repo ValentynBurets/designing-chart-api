75dfd9390f5a06b07e5a76b3a02fbae48ccd1524
valentyn.burets <Valentyn.Burets@gmail.com>
2021-11-23 20:54:36 +0200

    added dependency ingection to services

--- designing-chart-api/Configurations/DependencyInjection.cs
+++ designing-chart-api/Configurations/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Data.Contract.UnitOfWork;
+﻿using Business.Contract.Services;
+using Business.Services;
+using Data.Contract.UnitOfWork;
 using Data.UnitOfWork;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,9 @@ namespace designing_chart_api.Configurations
         public static IServiceCollection AddRepository(this IServiceCollection service)
         {
             service.AddTransient<IExerciseUnitOfWork, ExerciseUnitOfWork>();
-
+            service.AddTransient<IAttemptService, AttemptService>();
+            service.AddTransient<IExerciseService, ExerciseService>();
+            service.AddTransient<ICategoryService, CategoryService>();
             return service;
         }
     }
